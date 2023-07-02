ceb611df155d7f41647f8ace05caf880ea0d7d8c
Coding Streams <akxhdev@outlook.com>
2023-07-02 16:16:44 +0530

    add practice class for filter, map and reduce method

---
+++ src/streams/FilterMethodPractice.java
@@ -0,0 +1,15 @@
+package streams;
+
+public class FilterMethodPractice {
+    public static void main(String[] args) {
+        // Generate random list of numbers
+
+        // Print original list
+
+        // Filter odd numbers
+
+        // Filter numbers greater than 10
+
+        // Filter prime numbers
+    }
+}
